d6a98639ecb48ddf705065ec63a35c0943782504
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2013-05-07 15:11:24 +0100

    Updated sync test.

--- src/maidsafe/vault/tests/sync_test.cc
+++ src/maidsafe/vault/tests/sync_test.cc
@@ -14,8 +14,8 @@
 
 #include "boost/filesystem/operations.hpp"
 #include "boost/filesystem/path.hpp"
-#include "boost/progress.hpp"
 
+#include "maidsafe/common/error.h"
 #include "maidsafe/common/test.h"
 #include "maidsafe/common/utils.h"
 
@@ -32,58 +32,57 @@ namespace maidsafe {
 namespace vault {
 namespace test {
 
-const uint64_t kValueSize(36);
-
 namespace {
-  DataNameVariant GetRandomKey() {
-    // Currently 15 types are defined, but...
-    uint32_t number_of_types = boost::mpl::size<typename DataNameVariant::types>::type::value,
-             type_number;
-    type_number = RandomUint32() % number_of_types;
-    switch (type_number) {
-      case  0: return passport::Anmid::name_type();
-      case  1: return passport::Ansmid::name_type();
-      case  2: return passport::Antmid::name_type();
-      case  3: return passport::Anmaid::name_type();
-      case  4: return passport::Maid::name_type();
-      case  5: return passport::Pmid::name_type();
-      case  6: return passport::Mid::name_type();
-      case  7: return passport::Smid::name_type();
-      case  8: return passport::Tmid::name_type();
-      case  9: return passport::Anmpid::name_type();
-      case 10: return passport::Mpid::name_type();
-      case 11: return ImmutableData::name_type();
-      case 12: return OwnerDirectory::name_type();
-      case 13: return GroupDirectory::name_type();
-      case 14: return WorldDirectory::name_type();
-      // default:
-        // Throw something!
-      //  ;
-    }
-    return DataNameVariant();
+
+DataNameVariant GetRandomKey() {
+  // Currently 15 types are defined, but...
+  uint32_t number_of_types = boost::mpl::size<typename DataNameVariant::types>::type::value,
+            type_number;
+  type_number = RandomUint32() % number_of_types;
+  Identity id(RandomString(crypto::SHA512::DIGESTSIZE));
+  switch (type_number) {
+    case  0: return passport::Anmid::name_type(id);
+    case  1: return passport::Ansmid::name_type(id);
+    case  2: return passport::Antmid::name_type(id);
+    case  3: return passport::Anmaid::name_type(id);
+    case  4: return passport::Maid::name_type(id);
+    case  5: return passport::Pmid::name_type(id);
+    case  6: return passport::Mid::name_type(id);
+    case  7: return passport::Smid::name_type(id);
+    case  8: return passport::Tmid::name_type(id);
+    case  9: return passport::Anmpid::name_type(id);
+    case 10: return passport::Mpid::name_type(id);
+    case 11: return ImmutableData::name_type(id);
+    case 12: return OwnerDirectory::name_type(id);
+    case 13: return GroupDirectory::name_type(id);
+    case 14: return WorldDirectory::name_type(id);
+    default: ThrowError(CommonErrors::invalid_parameter);
   }
-
-  struct GenerateKeyValuePair : public boost::static_visitor<NonEmptyString>
-  {
-    GenerateKeyValuePair() {}
-
-    template<typename T>
-    NonEmptyString operator()(T& key) {
-      protobuf::MaidAccountDbValue proto_db_value;
-      proto_db_value.set_average_cost(33);
-      proto_db_value.set_count(2);
-      NonEmptyString value(proto_db_value.SerializeAsString());
-
-      key.data = Identity(crypto::Hash<crypto::SHA512>(value));
-      return value;
-    }
-  };
-
-  NonEmptyString GenerateKeyValueData(DataNameVariant& key) {
-    GenerateKeyValuePair generate_key_value_pair_;
-    return boost::apply_visitor(generate_key_value_pair_, key);
-  }
+  return DataNameVariant();
 }
+
+struct GenerateKeyValuePair : public boost::static_visitor<NonEmptyString> {
+  GenerateKeyValuePair() {}
+
+  template<typename T>
+  NonEmptyString operator()(T& key) {
+    protobuf::MaidAccountDbValue proto_db_value;
+    proto_db_value.set_average_cost(33);
+    proto_db_value.set_count(2);
+    NonEmptyString value(proto_db_value.SerializeAsString());
+
+    key.data = Identity(crypto::Hash<crypto::SHA512>(value));
+    return value;
+  }
+};
+
+NonEmptyString GenerateKeyValueData(DataNameVariant& key) {
+  GenerateKeyValuePair generate_key_value_pair;
+  return boost::apply_visitor(generate_key_value_pair, key);
+}
+
+}  // unnamed namespace
+
 
 template <typename MergePolicy>
 class SyncTest : public testing::Test {
@@ -91,7 +90,7 @@ class SyncTest : public testing::Test {
   SyncTest()
       : kTestRoot_(maidsafe::test::CreateTestPath("MaidSafe_Test_Vault")),
         vault_root_directory_(*kTestRoot_ / RandomAlphaNumericString(8)),
-        node_id_(RandomString(64)),
+        node_id_(NodeId::kRandomId),
         db_(vault_root_directory_),
         account_db_(db_),
         sync_(&account_db_, node_id_) {
@@ -102,8 +101,7 @@ class SyncTest : public testing::Test {
   typename MergePolicy::UnresolvedEntry CreateUnresolvedEntry(nfs::MessageAction action,
                                                               DataNameVariant key,
                                                               NodeId node_id) {
-    return typename MergePolicy::UnresolvedEntry(std::make_pair(key, action),
-                                                 32, node_id);
+    return typename MergePolicy::UnresolvedEntry(std::make_pair(key, action), 32, node_id);
   }
 
   const maidsafe::test::TestPath kTestRoot_;
@@ -130,32 +128,34 @@ TYPED_TEST_P(SyncTest, BEH_AddUnresolvedEntry) {
     typename TypeParam::UnresolvedEntry unresolved_entry(
         this->CreateUnresolvedEntry(nfs::MessageAction::kPut,
                                     GetRandomKey(),
-                                    NodeId(RandomString(64))));
-    EXPECT_FALSE(this->sync_.AddUnresolvedEntry(unresolved_entry));
-    EXPECT_TRUE(this->sync_.AddUnresolvedEntry(unresolved_entry));
+                                    NodeId(NodeId::kRandomId)));
+    for (int i(0); i != 100; ++i)
+      EXPECT_FALSE(this->sync_.AddUnresolvedEntry(unresolved_entry));
   }
-//   {
-//     typename TypeParam::UnresolvedEntry unresolved_entry(
-//         this->CreateUnresolvedEntry(nfs::MessageAction::kPut, GetRandomKey(), this->node_id_));
-//     EXPECT_FALSE(this->sync_.AddUnresolvedEntry(unresolved_entry));
-//     // assertion failure
-//     EXPECT_THROW(this->sync_.AddUnresolvedEntry(unresolved_entry));
-//   }
+  {
+    typename TypeParam::UnresolvedEntry unresolved_entry(
+        this->CreateUnresolvedEntry(nfs::MessageAction::kPut, GetRandomKey(), this->node_id_));
+    EXPECT_FALSE(this->sync_.AddUnresolvedEntry(unresolved_entry));
+#ifndef NDEBUG
+    EXPECT_DEATH(this->sync_.AddUnresolvedEntry(unresolved_entry), "");
+#else
+    EXPECT_FALSE(this->sync_.AddUnresolvedEntry(unresolved_entry));
+#endif
+  }
   {
     DataNameVariant key(GetRandomKey());
-    NonEmptyString value(GenerateKeyValueData(key));
-    this->account_db_.Put(std::make_pair(key, value));
-
     typename TypeParam::UnresolvedEntry unresolved_entry(
-        this->CreateUnresolvedEntry(nfs::MessageAction::kPut, key, NodeId(RandomString(64))));
-    for (size_t i(0); i < ((routing::Parameters::node_group_size + 1U) / 2 - 1); ++i) {
-      unresolved_entry.messages_contents.emplace_back();
-      unresolved_entry.messages_contents.back().peer_id = NodeId(RandomString(64));
-      unresolved_entry.messages_contents.back().entry_id = RandomInt32();
-      unresolved_entry.messages_contents.back().value = (RandomInt32() % 64 + 16);
+        this->CreateUnresolvedEntry(nfs::MessageAction::kPut, key, NodeId(NodeId::kRandomId)));
+    unresolved_entry.messages_contents.front().entry_id = RandomInt32();
+
+    for (size_t i(0); i < routing::Parameters::node_group_size / 2; ++i) {
+      EXPECT_FALSE(this->sync_.AddUnresolvedEntry(unresolved_entry));
+      // Set up unresolved_entry as though sent from a different peer.
+      unresolved_entry.messages_contents.front().peer_id = NodeId(NodeId::kRandomId);
+      unresolved_entry.messages_contents.front().entry_id = RandomInt32();
+      unresolved_entry.messages_contents.front().value = (RandomInt32() % 64 + 16);
     }
 
-    EXPECT_FALSE(this->sync_.AddUnresolvedEntry(unresolved_entry));
     EXPECT_TRUE(this->sync_.AddUnresolvedEntry(unresolved_entry));
   }
 }
@@ -163,8 +163,8 @@ TYPED_TEST_P(SyncTest, BEH_AddUnresolvedEntry) {
 REGISTER_TYPED_TEST_CASE_P(SyncTest, BEH_GetUnresolvedCount,
                                      BEH_AddUnresolvedEntry);
 
-typedef ::testing::Types<MaidAccountMergePolicy,
-                         PmidAccountMergePolicy> MergePolicies;
+typedef testing::Types<MaidAccountMergePolicy,
+                       PmidAccountMergePolicy> MergePolicies;
 INSTANTIATE_TYPED_TEST_CASE_P(Sync, SyncTest, MergePolicies);
 
 }  // namespace test

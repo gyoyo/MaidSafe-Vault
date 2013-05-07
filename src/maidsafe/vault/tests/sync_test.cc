f2d8108515502c771dc1cd45ef9396170275ce94
maqi <qi.ma@maidsafe.net>
2013-05-07 11:12:22 +0100

    extend BEH_AddUnresolvedEntry test [42087370]

--- src/maidsafe/vault/tests/sync_test.cc
+++ src/maidsafe/vault/tests/sync_test.cc
@@ -26,6 +26,7 @@
 #include "maidsafe/vault/sync.h"
 #include "maidsafe/vault/maid_account_holder/maid_account_merge_policy.h"
 #include "maidsafe/vault/pmid_account_holder/pmid_account_merge_policy.h"
+#include "maidsafe/vault/maid_account_holder/maid_account.pb.h"
 
 namespace maidsafe {
 namespace vault {
@@ -61,6 +62,27 @@ namespace {
     }
     return DataNameVariant();
   }
+
+  struct GenerateKeyValuePair : public boost::static_visitor<NonEmptyString>
+  {
+    GenerateKeyValuePair() {}
+
+    template<typename T>
+    NonEmptyString operator()(T& key) {
+      protobuf::MaidAccountDbValue proto_db_value;
+      proto_db_value.set_average_cost(33);
+      proto_db_value.set_count(2);
+      NonEmptyString value(proto_db_value.SerializeAsString());
+
+      key.data = Identity(crypto::Hash<crypto::SHA512>(value));
+      return value;
+    }
+  };
+
+  NonEmptyString GenerateKeyValueData(DataNameVariant& key) {
+    GenerateKeyValuePair generate_key_value_pair_;
+    return boost::apply_visitor(generate_key_value_pair_, key);
+  }
 }
 
 template <typename MergePolicy>
@@ -69,20 +91,24 @@ class SyncTest : public testing::Test {
   SyncTest()
       : kTestRoot_(maidsafe::test::CreateTestPath("MaidSafe_Test_Vault")),
         vault_root_directory_(*kTestRoot_ / RandomAlphaNumericString(8)),
+        node_id_(RandomString(64)),
         db_(vault_root_directory_),
         account_db_(db_),
-        sync_(&account_db_, NodeId(RandomString(64))) {
+        sync_(&account_db_, node_id_) {
     boost::filesystem::create_directory(vault_root_directory_);
   }
 
  protected:
-  typename MergePolicy::UnresolvedEntry CreateUnresolvedEntry(nfs::MessageAction action) {
-    return typename MergePolicy::UnresolvedEntry(std::make_pair(GetRandomKey(), action),
-                                                 32, NodeId(RandomString(64)));
+  typename MergePolicy::UnresolvedEntry CreateUnresolvedEntry(nfs::MessageAction action,
+                                                              DataNameVariant key,
+                                                              NodeId node_id) {
+    return typename MergePolicy::UnresolvedEntry(std::make_pair(key, action),
+                                                 32, node_id);
   }
 
   const maidsafe::test::TestPath kTestRoot_;
   boost::filesystem::path vault_root_directory_;
+  NodeId node_id_;
   Db db_;
   AccountDb account_db_;
   Sync<MergePolicy> sync_;
@@ -102,7 +128,34 @@ TYPED_TEST_P(SyncTest, BEH_AddUnresolvedEntry) {
   }
   {
     typename TypeParam::UnresolvedEntry unresolved_entry(
-        this->CreateUnresolvedEntry(nfs::MessageAction::kPut));
+        this->CreateUnresolvedEntry(nfs::MessageAction::kPut,
+                                    GetRandomKey(),
+                                    NodeId(RandomString(64))));
+    EXPECT_FALSE(this->sync_.AddUnresolvedEntry(unresolved_entry));
+    EXPECT_TRUE(this->sync_.AddUnresolvedEntry(unresolved_entry));
+  }
+//   {
+//     typename TypeParam::UnresolvedEntry unresolved_entry(
+//         this->CreateUnresolvedEntry(nfs::MessageAction::kPut, GetRandomKey(), this->node_id_));
+//     EXPECT_FALSE(this->sync_.AddUnresolvedEntry(unresolved_entry));
+//     // assertion failure
+//     EXPECT_THROW(this->sync_.AddUnresolvedEntry(unresolved_entry));
+//   }
+  {
+    DataNameVariant key(GetRandomKey());
+    NonEmptyString value(GenerateKeyValueData(key));
+    this->account_db_.Put(std::make_pair(key, value));
+
+    typename TypeParam::UnresolvedEntry unresolved_entry(
+        this->CreateUnresolvedEntry(nfs::MessageAction::kPut, key, NodeId(RandomString(64))));
+    for (size_t i(0); i < ((routing::Parameters::node_group_size + 1U) / 2 - 1); ++i) {
+      unresolved_entry.messages_contents.emplace_back();
+      unresolved_entry.messages_contents.back().peer_id = NodeId(RandomString(64));
+      unresolved_entry.messages_contents.back().entry_id = RandomInt32();
+      unresolved_entry.messages_contents.back().value = (RandomInt32() % 64 + 16);
+    }
+
+    EXPECT_FALSE(this->sync_.AddUnresolvedEntry(unresolved_entry));
     EXPECT_TRUE(this->sync_.AddUnresolvedEntry(unresolved_entry));
   }
 }
